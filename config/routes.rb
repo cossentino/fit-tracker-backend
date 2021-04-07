fdad7a0ab2c9e6a77417d7e119535aa3fa4863ed
Ian Cossentino <icossentino@gmail.com>
2021-04-07 15:28:46 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show, :update]
+      resources :users do
+        resources :workouts
+      end
     end
   end
 
