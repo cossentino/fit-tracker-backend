d99e572d18b1c589e6c82b1aeb9f151a5f9a7365
Ian Cossentino <icossentino@gmail.com>
2021-04-07 15:23:26 -0400

    added seed data for workouts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+workouts = [{
+  date: '2021-03-02',
+  workout_type: 'swim',
+  exert: 9,
+  miles: 3,
+  location: "Boston, MA",
+  notes: "tough swim"},
+
+  {
+    date: '2020-12-31',
+    workout_type: 'bike',
+    exert: 5,
+    miles: 15,
+    pace: '05:00',
+    location: "Winchester, MA",
+    notes: "easy bike ride through the fells"},
+  
+    {
+      date: '2021-04-01',
+      workout_type: 'run',
+      exert: 8,
+      miles: 10,
+      pace: '07:56',
+      location: "Boston, MA",
+      notes: "weekend long run"},
+    
+      {
+        date: '2021-03-15',
+        workout_type: 'swim',
+        exert: 3,
+        miles: 1,
+        pace: '12:00',
+        location: "Boston, MA",
+        notes: "morning swim"}]
+
+workouts.each do |w|
+  wo = User.first.workouts.build(w)
+  wo.save
+end
