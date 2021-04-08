8043dce579c4a7ac7945b4d2b7578367698cbdc0
Ian Cossentino <icossentino@gmail.com>
2021-04-08 17:29:30 -0400

    added create method to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::UsersController < ActionController::API
 
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: { user: UsersSerializer.new(@user) }, status: :accepted
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
+  
+  
+  
   def index
     @users = User.all
     render json: UsersSerializer.new(@users)
