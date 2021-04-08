703ce097cb79018e0f19fa09544f74d775aa5cf1
Ian Cossentino <icossentino@gmail.com>
2021-04-08 18:09:47 -0400

    added auth controller and user profile method to backend

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users do
-        post '/login', to: 'users#create'
+        post '/login', to: 'auth#create'
+        get '/profile', to: 'users#profile'
         resources :workouts
       end
     end
